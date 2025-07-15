3f781ef7185293f69fe2b7682b34ad3f557f94d8
josesbaraini <josesbarainips@gmail.com>
2025-07-15 11:02:25 -0300

    Adicionando view de detalhes de um endereço

--- App/urls.py
+++ App/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
-from adress.views import adress_view
+from adress.views import adress_create_list_view, adress_datail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('adresses/', adress_view, name='adress'),
+    path('adresses/', adress_create_list_view, name='adress'),
+    path('adresses/<int:pk>/', adress_datail_view, name='adress-datauls-view'),
 
 
 ]
--- adress/views.py
+++ adress/views.py
@@ -1,10 +1,11 @@
 from django.http import JsonResponse
 from adress.models import Adress
 import json
+from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
-def adress_view(request):
+def adress_create_list_view(request):
     if request.method == 'GET':
         adresses = Adress.objects.all()
         date = [{'id':adress.id,'type':adress.type, 'state_registration':adress.state_registration,'postal_code':adress.postal_code, 'address':adress.address} for adress in adresses]
@@ -28,4 +29,21 @@ def adress_view(request):
         return JsonResponse(
             {'id':new_adress.id, 'adress': new_adress.address}, status=201
         )
-
+@csrf_exempt
+def adress_datail_view(request, pk):
+    adress = get_object_or_404(Adress, pk=pk)
+    date = {
+    'id': adress.id,
+    'type': adress.type,
+    'state_registration': adress.state_registration,
+    'postal_code': adress.postal_code,
+    'address': adress.address,
+    'address_complement': adress.address_complement,
+    'neighborhood': adress.neighborhood,
+    'city': adress.city,
+    'area_code': adress.area_code,
+    'state_abbr': adress.state_abbr,
+    'state': adress.state,
+    'ibge_code': adress.ibge_code
+}
+    return JsonResponse(date)
