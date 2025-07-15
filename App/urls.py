0b64856043adbd7b144c38aea4a2d274ba2f8360
josesbaraini <josesbarainips@gmail.com>
2025-07-15 10:48:08 -0300

    Adicionando a view Adress, pra fazer consultas e cadastros no banco de dados

--- App/urls.py
+++ App/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
+from adress.views import adress_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('adresses/', adress_view, name='adress'),
+
 
 ]
--- adress/views.py
+++ adress/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from adress.models import Adress
+import json
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
+def adress_view(request):
+    if request.method == 'GET':
+        adresses = Adress.objects.all()
+        date = [{'id':adress.id,'type':adress.type, 'state_registration':adress.state_registration,'postal_code':adress.postal_code, 'address':adress.address} for adress in adresses]
+        return JsonResponse(date, safe=False)
+    elif request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        new_adress = Adress.objects.create(
+            type=data['type'],  
+            state_registration=data['state_registration'],
+            postal_code=data['postal_code'],
+            address=data['address'],
+            address_complement = data['address_complement'],
+            neighborhood = data['neighborhood'],
+            city = data['city'],
+            area_code = data['area_code'],
+            state_abbr = data['state_abbr'],
+            state = data['state'],
+            ibge_code = data['ibge_code'],
+        )
+        new_adress.save()
+        return JsonResponse(
+            {'id':new_adress.id, 'adress': new_adress.address}, status=201
+        )
+
