9faa529eea823533d8a639c8b2c93cd1e20f7a6f
josesbaraini <josesbarainips@gmail.com>
2025-07-17 11:24:08 -0300

    exemplo de media

--- client/serializers.py
+++ client/serializers.py
@@ -6,7 +6,19 @@ from phone.serializers import PhoneSerializer
 class ClientModelSerializer(serializers.ModelSerializer):
     emails = EmailSerializer(many=True, read_only=True)
     phones = PhoneSerializer(many=True, read_only=True)
+    rate = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Client
         fields= "__all__"
+
+    def get_rate(self, obj):
+        reviews = obj.reviews.all()
+        if reviews:
+            sum_reviews = 0
+            for review in reviews:
+                sum_reviews += review.stars
+            return round(sum_reviews / reviews.count(), 1)
+        
+
+        return None
